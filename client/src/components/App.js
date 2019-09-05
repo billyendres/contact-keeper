01c6aa034bbda24b5b7950ae73fa09dd159b6eef
Billy <endres63@hotmail.com>
2019-09-05 11:15:31 +1000

    Server initialized

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>app</h1>
+    </div>
+  );
+};
+
+export default App;
+
+// mongodb+srv://billy:billy@cluster0-f1wyg.mongodb.net/test?retryWrites=true&w=majority
